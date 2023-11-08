9db7a50b71cc8542211984ae02470f4b2a0cea5d
Adenhall <jamesadenhall@gmail.com>
2023-11-08 13:03:39 +0700

    feat: first success subDAO deployment

---
+++ contracts/GroupVotingPlugin/GroupVoting.sol
@@ -0,0 +1,322 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity ^0.8.17;
+
+import {SafeCastUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/math/SafeCastUpgradeable.sol";
+
+import {IDAO} from "@aragon/osx/core/dao/IDAO.sol";
+import {RATIO_BASE, _applyRatioCeiled} from "@aragon/osx/plugins/utils/Ratio.sol";
+
+import {IMembership} from "@aragon/osx/core/plugin/membership/IMembership.sol";
+import {IMajorityVoting} from "@aragon/osx/plugins/governance/majority-voting/IMajorityVoting.sol";
+import {MajorityVotingBase} from "@aragon/osx/plugins/governance/majority-voting/MajorityVotingBase.sol";
+import {GroupVotingList} from "./GroupVotingList.sol";
+import {Vault} from "./Vault.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/// @title GroupVoting
+/// @notice The majority voting implementation using groups of members
+/// @dev This contract inherits from `MajorityVotingBase` and implements the `IMajorityVoting` interface.
+contract GroupVoting is IMembership, MajorityVotingBase {
+    using SafeCastUpgradeable for uint256;
+    using Counters for Counters.Counter;
+
+    /// @notice The [ERC-165](https://eips.ethereum.org/EIPS/eip-165) interface ID of the contract.
+    bytes4 internal constant GROUPLIST_VOTING_INTERFACE_ID =
+        this.initialize.selector ^
+            this.addAddresses.selector ^
+            this.removeAddresses.selector;
+
+    /// @notice The ID of the permission required to call the `addAddresses` and `removeAddresses` functions.
+    bytes32 public constant UPDATE_ADDRESSES_PERMISSION_ID =
+        keccak256("UPDATE_ADDRESSES_PERMISSION");
+
+    bytes32 public constant CREATE_GROUP_PERMISSION_ID =
+        keccak256("CREATE_GROUP_PERMISSION");
+
+    Counters.Counter public _groupIdCounter;
+    mapping(uint256 => string) public groupsNames;
+    mapping(uint256 => GroupVotingList) public groups;
+    mapping(uint256 => Vault) public groupVault;
+    mapping(uint256 => uint256) public proposalGroup;
+
+    /// @notice Initializes the component.
+    /// @dev This method is required to support [ERC-1822](https://eips.ethereum.org/EIPS/eip-1822).
+    /// @param _dao The IDAO interface of the associated DAO.
+    /// @param _votingSettings The voting settings.
+    function initialize(
+        IDAO _dao,
+        VotingSettings calldata _votingSettings
+    ) external initializer {
+        __MajorityVotingBase_init(_dao, _votingSettings);
+    }
+ 
+    function createGroup(
+        string calldata _groupName,
+        address[] calldata _members,
+        address _tokenAllocation,
+        uint256 _initialAllocation
+    ) external auth(CREATE_GROUP_PERMISSION_ID) {
+        uint256 groupId = _groupIdCounter.current();
+        _groupIdCounter.increment();
+
+        GroupVotingList group = new GroupVotingList();
+        group.addAddresses(_members);
+
+        groupsNames[groupId] = _groupName;
+        groups[groupId] = group;
+
+        Vault vault = new Vault();
+        groupVault[groupId] = vault;
+
+        ERC20(_tokenAllocation).transfer(address(vault), _initialAllocation);
+
+        emit MembersAdded({members: _members});
+    }
+
+    /// @notice Adds new members to the address list.
+    /// @param _members The addresses of members to be added.
+    /// @dev This function is used during the plugin initialization.
+    function addAddresses(
+        address[] calldata _members,
+        uint256 _groupId
+    ) external auth(UPDATE_ADDRESSES_PERMISSION_ID) {
+        groups[_groupId].addAddresses(_members);
+
+        emit MembersAdded({members: _members});
+    }
+
+    /// @notice Removes existing members from the address list.
+    /// @param _members The addresses of the members to be removed.
+    function removeAddresses(
+        address[] calldata _members,
+        uint256 _groupId
+    ) external auth(UPDATE_ADDRESSES_PERMISSION_ID) {
+        groups[_groupId].removeAddresses(_members);
+
+        emit MembersRemoved({members: _members});
+    }
+
+    /// @inheritdoc MajorityVotingBase
+
+    function totalVotingPower(
+        uint256 _blockNumber
+    ) public view override returns (uint256) {}
+
+    function totalVotingPower(
+        uint256 _blockNumber,
+        uint256 _groupId
+    ) public view returns (uint256) {
+        return groups[_groupId].addresslistLengthAtBlock(_blockNumber);
+    }
+
+    function isListedAtBlock(
+        address _account,
+        uint256 _groupId,
+        uint256 _blockNumber
+    ) public view virtual returns (bool) {
+        return groups[_groupId].isListedAtBlock(_account, _blockNumber);
+    }
+
+    /// @inheritdoc MajorityVotingBase
+    function createProposal(
+        bytes calldata _metadata,
+        IDAO.Action[] calldata _actions,
+        uint256 _allowFailureMap,
+        uint64 _startDate,
+        uint64 _endDate,
+        VoteOption _voteOption,
+        bool _tryEarlyExecution
+    ) external override returns (uint256 proposalId) {}
+
+    function createProposal(
+        bytes calldata _metadata,
+        IDAO.Action[] calldata _actions,
+        uint256 _allowFailureMap,
+        uint64 _startDate,
+        uint64 _endDate,
+        VoteOption _voteOption,
+        bool _tryEarlyExecution,
+        uint256 _groupId
+    ) external returns (uint256 proposalId) {
+        uint64 snapshotBlock;
+        unchecked {
+            snapshotBlock = block.number.toUint64() - 1;
+        }
+
+        if (
+            minProposerVotingPower() != 0 &&
+            !groups[_groupId].isListedAtBlock(_msgSender(), snapshotBlock)
+        ) {
+            revert ProposalCreationForbidden(_msgSender());
+        }
+
+        proposalId = _createProposal({
+            _creator: _msgSender(),
+            _metadata: _metadata,
+            _startDate: _startDate,
+            _endDate: _endDate,
+            _actions: _actions,
+            _allowFailureMap: _allowFailureMap
+        });
+
+        // Store proposal related information
+        Proposal storage proposal_ = proposals[proposalId];
+
+        proposalGroup[proposalId] = _groupId;
+
+        (
+            proposal_.parameters.startDate,
+            proposal_.parameters.endDate
+        ) = _validateProposalDates({_start: _startDate, _end: _endDate});
+        proposal_.parameters.snapshotBlock = snapshotBlock;
+        proposal_.parameters.votingMode = votingMode();
+        proposal_.parameters.supportThreshold = supportThreshold();
+        proposal_.parameters.minVotingPower = _applyRatioCeiled(
+            totalVotingPower(snapshotBlock),
+            minParticipation()
+        );
+
+        // Reduce costs
+        if (_allowFailureMap != 0) {
+            proposal_.allowFailureMap = _allowFailureMap;
+        }
+
+        for (uint256 i; i < _actions.length; ) {
+            proposal_.actions.push(_actions[i]);
+            unchecked {
+                ++i;
+            }
+        }
+
+        if (_voteOption != VoteOption.None) {
+            vote(proposalId, _voteOption, _tryEarlyExecution);
+        }
+    }
+
+    function isMember(
+        address _account,
+        uint256 _groupId
+    ) public view returns (bool) {
+        return groups[_groupId].isListed(_account);
+    }
+
+    /// @inheritdoc IMembership
+    function isMember(address _account) external view returns (bool) {}
+
+    /// @inheritdoc MajorityVotingBase
+    function _vote(
+        uint256 _proposalId,
+        VoteOption _voteOption,
+        address _voter,
+        bool _tryEarlyExecution
+    ) internal override {
+        Proposal storage proposal_ = proposals[_proposalId];
+
+        VoteOption state = proposal_.voters[_voter];
+
+        // Remove the previous vote.
+        if (state == VoteOption.Yes) {
+            proposal_.tally.yes = proposal_.tally.yes - 1;
+        } else if (state == VoteOption.No) {
+            proposal_.tally.no = proposal_.tally.no - 1;
+        } else if (state == VoteOption.Abstain) {
+            proposal_.tally.abstain = proposal_.tally.abstain - 1;
+        }
+
+        // Store the updated/new vote for the voter.
+        if (_voteOption == VoteOption.Yes) {
+            proposal_.tally.yes = proposal_.tally.yes + 1;
+        } else if (_voteOption == VoteOption.No) {
+            proposal_.tally.no = proposal_.tally.no + 1;
+        } else if (_voteOption == VoteOption.Abstain) {
+            proposal_.tally.abstain = proposal_.tally.abstain + 1;
+        }
+
+        proposal_.voters[_voter] = _voteOption;
+
+        emit VoteCast({
+            proposalId: _proposalId,
+            voter: _voter,
+            voteOption: _voteOption,
+            votingPower: 1
+        });
+
+        if (_tryEarlyExecution && _canExecute(_proposalId)) {
+            _execute(_proposalId);
+        }
+    }
+
+    /// @inheritdoc MajorityVotingBase
+    function _canVote(
+        uint256 _proposalId,
+        address _account,
+        VoteOption _voteOption
+    ) internal view override returns (bool) {
+        Proposal storage proposal_ = proposals[_proposalId];
+
+        // The proposal vote hasn't started or has already ended.
+        if (!_isProposalOpen(proposal_)) {
+            return false;
+        }
+
+        // The voter votes `None` which is not allowed.
+        if (_voteOption == VoteOption.None) {
+            return false;
+        }
+
+        // The voter has no voting power.
+        if (
+            !groups[proposalGroup[_proposalId]].isListedAtBlock(
+                _account,
+                proposal_.parameters.snapshotBlock
+            )
+        ) {
+            return false;
+        }
+
+        // The voter has already voted but vote replacement is not allowed.
+        if (
+            proposal_.voters[_account] != VoteOption.None &&
+            proposal_.parameters.votingMode != VotingMode.VoteReplacement
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    function withdrawNFT(
+        address _tokenAddress,
+        uint256 _tokenId,
+        address _destination,
+        uint256 _groupId
+    ) external {
+        require(isMember(_msgSender(), _groupId), "Not a group member");
+        groupVault[_groupId].withdrawNFT(_tokenAddress, _tokenId, _destination);
+    }
+
+    function withdrawERC20(
+        address _tokenAddress,
+        uint256 _amount,
+        address _destination,
+        uint256 _groupId
+    ) external {
+        require(isMember(_msgSender(), _groupId), "Not a group member");
+        groupVault[_groupId].withdrawERC20(
+            _tokenAddress,
+            _amount,
+            _destination
+        );
+    }
+
+    function getGroupVault(uint256 _groupId) external view returns (address) {
+        return address(groupVault[_groupId]);
+    }
+
+    /// @dev This empty reserved space is put in place to allow future versions to add new
+    /// variables without shifting down storage in the inheritance chain.
+    /// https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+    uint256[50] private __gap;
+}
---
+++ contracts/GroupVotingPlugin/GroupVotingSetup.sol
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity ^0.8.17;
+
+import {IDAO} from "@aragon/osx/core/dao/IDAO.sol";
+import {DAO} from "@aragon/osx/core/dao/DAO.sol";
+import {PermissionLib} from "@aragon/osx/core/permission/PermissionLib.sol";
+import {PluginSetup, IPluginSetup} from "@aragon/osx/framework/plugin/setup/PluginSetup.sol";
+import {MajorityVotingBase} from "@aragon/osx/plugins/governance/majority-voting/MajorityVotingBase.sol";
+import {GroupVoting} from "./GroupVoting.sol";
+
+/// @title GroupVotingSetup
+/// @author Aragon Association - 2022-2023
+/// @notice The setup contract of the `AddresslistVoting` plugin.
+contract GroupVotingSetup is PluginSetup {
+    /// @notice The address of `AddresslistVoting` plugin logic contract to be used in creating proxy contracts.
+    GroupVoting private immutable groupVotingBase;
+
+    /// @notice The contract constructor, that deployes the `AddresslistVoting` plugin logic contract.
+    constructor() {
+        groupVotingBase = new GroupVoting();
+    }
+
+    /// @inheritdoc IPluginSetup
+    function prepareInstallation(
+        address _dao,
+        bytes calldata _data
+    ) external returns (address plugin, PreparedSetupData memory preparedSetupData) {
+        // Decode `_data` to extract the params needed for deploying and initializing `AddresslistVoting` plugin.
+        (MajorityVotingBase.VotingSettings memory votingSettings) = abi
+            .decode(_data, (MajorityVotingBase.VotingSettings));
+
+        // Prepare and Deploy the plugin proxy.
+        plugin = createERC1967Proxy(
+            address(groupVotingBase),
+            abi.encodeWithSelector(
+                GroupVoting.initialize.selector,
+                _dao,
+                votingSettings
+            )
+        );
+
+        // Prepare permissions
+        PermissionLib.MultiTargetPermission[]
+            memory permissions = new PermissionLib.MultiTargetPermission[](4);
+
+        // Set permissions to be granted.
+        // Grant the list of prmissions of the plugin to the DAO.
+        permissions[0] = PermissionLib.MultiTargetPermission(
+            PermissionLib.Operation.Grant,
+            plugin,
+            _dao,
+            PermissionLib.NO_CONDITION,
+            groupVotingBase.UPDATE_ADDRESSES_PERMISSION_ID()
+        );
+
+        permissions[1] = PermissionLib.MultiTargetPermission(
+            PermissionLib.Operation.Grant,
+            plugin,
+            _dao,
+            PermissionLib.NO_CONDITION,
+            groupVotingBase.UPDATE_VOTING_SETTINGS_PERMISSION_ID()
+        );
+
+        permissions[2] = PermissionLib.MultiTargetPermission(
+            PermissionLib.Operation.Grant,
+            plugin,
+            _dao,
+            PermissionLib.NO_CONDITION,
+            groupVotingBase.UPGRADE_PLUGIN_PERMISSION_ID()
+        );
+
+        // Grant `EXECUTE_PERMISSION` of the DAO to the plugin.
+        permissions[3] = PermissionLib.MultiTargetPermission(
+            PermissionLib.Operation.Grant,
+            _dao,
+            plugin,
+            PermissionLib.NO_CONDITION,
+            DAO(payable(_dao)).EXECUTE_PERMISSION_ID()
+        );
+
+        preparedSetupData.permissions = permissions;
+    }
+
+    /// @inheritdoc IPluginSetup
+    function prepareUninstallation(
+        address _dao,
+        SetupPayload calldata _payload
+    ) external view returns (PermissionLib.MultiTargetPermission[] memory permissions) {
+        // Prepare permissions
+        permissions = new PermissionLib.MultiTargetPermission[](4);
+
+        // Set permissions to be Revoked.
+        permissions[0] = PermissionLib.MultiTargetPermission(
+            PermissionLib.Operation.Revoke,
+            _payload.plugin,
+            _dao,
+            PermissionLib.NO_CONDITION,
+            groupVotingBase.UPDATE_ADDRESSES_PERMISSION_ID()
+        );
+
+        permissions[1] = PermissionLib.MultiTargetPermission(
+            PermissionLib.Operation.Revoke,
+            _payload.plugin,
+            _dao,
+            PermissionLib.NO_CONDITION,
+            groupVotingBase.UPDATE_VOTING_SETTINGS_PERMISSION_ID()
+        );
+
+        permissions[2] = PermissionLib.MultiTargetPermission(
+            PermissionLib.Operation.Revoke,
+            _payload.plugin,
+            _dao,
+            PermissionLib.NO_CONDITION,
+            groupVotingBase.UPGRADE_PLUGIN_PERMISSION_ID()
+        );
+
+        permissions[3] = PermissionLib.MultiTargetPermission(
+            PermissionLib.Operation.Revoke,
+            _dao,
+            _payload.plugin,
+            PermissionLib.NO_CONDITION,
+            DAO(payable(_dao)).EXECUTE_PERMISSION_ID()
+        );
+    }
+
+    /// @inheritdoc IPluginSetup
+    function implementation() external view returns (address) {
+        return address(groupVotingBase);
+    }
+}
